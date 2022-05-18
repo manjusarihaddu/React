bb6fceba852c74283f1006bcab72963c7802df4a
manjusarihaddu <100715858+manjusarihaddu@users.noreply.github.com>
2022-05-18 22:42:07 +0530

    React-Mobile

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import Mobile from './components/Mobile';
+function App() {
+  return (
+    <div className="App">
+     <Mobile/>
+    </div>
+  );
+}
+
+export default App;
